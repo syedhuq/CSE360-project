29b236d7fdedfccf505f6f2133e6606986f68b49
nnikan <31676915+nnikan@users.noreply.github.com>
2017-09-07 19:53:26 -0700

    Update Tutor.java

--- Tutor.java
+++ Tutor.java
@@ -1,5 +1,35 @@
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JEditorPane;
+import javax.swing.JOptionPane;
+import java.io.*;
 
-public class Tutor
-{
+public class Tutor extends JPanel {
 
-}
+    int state = 0;
+    JLabel myName  = new JLabel("Nathan Nikan");
+    JEditorPane HtmlPane= new JEditorPane();
+
+    public Tutor() {
+	// Initial state of displaying name.
+	add(myName, HtmlPane);
+    }
+
+    public void changeState(int newState) {
+	// Set the state to the new state and set the correct file
+        state = newState;
+	File file1= new File("P" + state + ".html");
+
+	try {
+	        HtmlPane.setPage(file1.toURI().toURL());
+	}
+
+	catch (IOException ex) {
+	    JOptionPane.showMessageDialog(null,"Could not setPage ", "TITLE", JOptionPane.WARNING_MESSAGE);
+            ex.printStackTrace();
+	}
+	remove(myName);
+	add(HtmlPane);    
+    }
+
+} 
