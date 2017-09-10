98c47720be19ae3b6dbbaec4d61c08ead9b29b95
Dalton Scott <sharksc83@gmail.com>
2017-09-10 12:28:27 -0700

    Add files via upload

--- Assessor.java
+++ Assessor.java
@@ -1,3 +1,14 @@
+/**
+* A class that shows a question and can display different ways to answer it.
+* Recitation Project 1
+* Completion time: 1 hour
+*
+* @author Dalton Scott
+* @version 1.0
+*/
+
+
+
 //package srcc; //package declaration, used only for Eclipse
 
 import javax.swing.*;
@@ -5,82 +16,82 @@ import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Assessor extends JPanel implements ActionListener, ItemListener {                //class is a child of JPanel, and uses ActionListener and ItemListener to handle actions on answer choices
-	int state = 0;                                                                            //state of assessor panel
+public class Assessor extends JPanel implements ActionListener, ItemListener {
+	int state = 0; //state of assessor panel
 	
-	JLabel nameLabel = new JLabel("Dalton Scott"); 											  //label with name of program writer
+	JLabel nameLabel = new JLabel("Dalton Scott");
 	
-	JLabel questionLabel = new JLabel("Question 1");										  //label that displays question
+	JLabel questionLabel = new JLabel("Question 1");
 	
-	String[] dropDownChoices = {"Option 1", "Option 2", "Option 3"};						  //create an array with the choices for the drop down menu
-	JComboBox dropDownMenu = new JComboBox(dropDownChoices); 								  //create a new drop down menu with the choices in the above array
+	String[] dropDownChoices = {"Option 1", "Option 2", "Option 3"};
+	JComboBox dropDownMenu = new JComboBox(dropDownChoices);
 	
-	JCheckBox checkBox1 = new JCheckBox("Option 1"); 									      //create a new checkbox labelled Option 1
-	JCheckBox checkBox2 = new JCheckBox("Option 2");  										  //create a new checkbox labelled Option 2
-	JCheckBox checkBox3 = new JCheckBox("Option 3"); 										  //create a new checkbox labelled Option 3
+	JCheckBox checkBox1 = new JCheckBox("Option 1");
+	JCheckBox checkBox2 = new JCheckBox("Option 2");
+	JCheckBox checkBox3 = new JCheckBox("Option 3");
 	
-	JButton button1 = new JButton("Option 1"); 												  //create a new button with "Option 1" inside it
-	JButton button2 = new JButton("Option 2"); 												  //create a new button with "Option 2" inside it
-	JButton button3 = new JButton("Option 3"); 												  //create a new button with "Option 3" inside it
+	JButton button1 = new JButton("Option 1");
+	JButton button2 = new JButton("Option 2");
+	JButton button3 = new JButton("Option 3");
 	
-	JTextField textField = new JTextField(16); 												  //create a new text field with 16 character spaces
+	JTextField textField = new JTextField(16);
 	
-	public Assessor() { 																	  //assessor panel constructor
-		super(); 																			  //call JPanel constructor
-		dropDownMenu.setSelectedIndex(0); 													  //set default selected answer to first one
-		dropDownMenu.addActionListener(this); 												  //add action listener to pop up message when answer is selected
+	public Assessor() {
+		super();
+		dropDownMenu.setSelectedIndex(0);
+		dropDownMenu.addActionListener(this);
 		
-		checkBox1.setSelected(false); 														  //set check box to not be selected by default
-		checkBox1.addItemListener(this);  													  //add item listener to pop up message when answer is selected
-		checkBox2.setSelected(false); 											              //set check box to not be selected by default
-		checkBox2.addItemListener(this); 													  //add item listener to pop up message when answer is selected
-		checkBox3.setSelected(false); 											              //set check box to not be selected by default
-		checkBox3.addItemListener(this); 													  //add item listener to pop up message when answer is selected
+		checkBox1.setSelected(false);
+		checkBox1.addItemListener(this);
+		checkBox2.setSelected(false);
+		checkBox2.addItemListener(this);
+		checkBox3.setSelected(false);
+		checkBox3.addItemListener(this);
 		
-		button1.addActionListener(this); 													  //add action listener to pop up message when answer is selected
-		button2.addActionListener(this); 													  //add action listener to pop up message when answer is selected
-		button3.addActionListener(this); 													  //add action listener to pop up message when answer is selected
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
 		
-		textField.addActionListener(this); 												      //add action listener to pop up message when answer is entered into text box
+		textField.addActionListener(this);
 		
-		add(nameLabel); 																	  //add name to panel by default
+		add(nameLabel);
 	}
 	
-	public void changeState(int changeTo) { 												  //method to change the state of the panel (from Universe slider)
-		removeAll(); 																		  //remove all elements from panel
-		state = changeTo; 																	  //change state variable
-		questionLabel.setText("Question " + Integer.toString(state));                         //set question label to show the correct question
-		add(questionLabel); 																  //add question label back to panel
-		if (state == 1) { 																	  //if we are in the drop down menu state
-			add(dropDownMenu); 														          //add drop down menu to panel
+	public void changeState(int changeTo) {
+		removeAll();
+		state = changeTo;
+		questionLabel.setText("Question " + Integer.toString(state));
+		add(questionLabel);
+		if (state == 1) {
+			add(dropDownMenu);
 		}
 		else
-		if (state == 2) { 																	  //if we are in the check box state
-			add(checkBox1); 															      //add check box 1 to panel
-			add(checkBox2); 																  //add check box 2 to panel
-			add(checkBox3); 																  //add check box 3 to panel
+		if (state == 2) {
+			add(checkBox1);
+			add(checkBox2);
+			add(checkBox3);
 		}
 		else
-		if (state == 3) { 																	  //if we are in the button state
-			add(button1); 																      //add button 1 to panel
-			add(button2); 																	  //add button 2 to panel
-			add(button3); 																	  //add button 3 to panel
+		if (state == 3) {
+			add(button1);
+			add(button2);
+			add(button3);
 		}
 		else
-		if (state == 4) { 																	  //if we are in the text field state
-			add(textField); 																  //add text field to panel
+		if (state == 4) {
+			add(textField);
 		}
 	}
 	
-	public void popUp(String message, String title) { 										  //method to display a pop up message
-		JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE); //display a pop up message with the given title and message as an information message
+	public void popUp(String message, String title) {
+		JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
 	}
 	
-	public void actionPerformed(ActionEvent e) { 											  //if action is performed (for button, text field, and drop down menu)
-		popUp("Hello World!", "Simple Output"); 											  //show a message with message "Hello World!" and title "Simple Output"
+	public void actionPerformed(ActionEvent e) {
+		popUp("Hello World!", "Simple Output");
 	}
 	
-	public void itemStateChanged(ItemEvent i) { 											  //if state is changed (for check boxes)
-		popUp("Hello World!", "Simple Output"); 											  //show a message with message "Hello World!" and title "Simple Output"
+	public void itemStateChanged(ItemEvent i) {
+		popUp("Hello World!", "Simple Output");
 	}
 }
